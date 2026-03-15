cc88394bf95efdeef566e4c81bd354288b3f974d
Riyon Aryono <riyon@riyonaryono.net>
2026-03-15 13:41:20 +0700

    add branch and loop

--- cmd/syntax/branching/main.go
+++ cmd/syntax/branching/main.go
@@ -1,31 +1,43 @@
 package main
 
-import (
-	"essentials/pkg/util"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	util.PrintHeader("Branching")
-	var point = 10
+	// var point = 8
+	//
+	// if point == 10 {
+	// 	fmt.Println("lulus dengan nilai sempuarna")
+	// } else if point > 5 {
+	// 	fmt.Println("lulus")
+	// } else if point == 4 {
+	// 	fmt.Println("hampir lulus")
+	// } else {
+	// 	fmt.Printf("tidak lulus. nilai anda %d\n", point)
+	// }
 
-	if point > 7 {
-		switch point {
-		case 10:
-			fmt.Println("perfect!")
-		default:
-			fmt.Println("nice!")
-		}
-	} else {
-		if point == 5 {
-			fmt.Println("not bad")
-		} else if point == 3 {
-			fmt.Println("keep trying")
-		} else {
-			fmt.Println("you can do it")
-			if point == 0 {
-				fmt.Println("try harder!")
-			}
-		}
+	// variable temporary
+
+	// var point = 10000.0
+	//
+	// if percent := point / 100; percent >= 100 {
+	// 	fmt.Printf("%.1f%s perceft!\n", percent, "%")
+	// } else if percent >= 70 {
+	// 	fmt.Printf("%.1f%s good\n", percent, "%")
+	// } else {
+	// 	fmt.Printf("%.1f%s not bad\n", percent, "%")
+	// }
+
+	// switch
+
+	var point = 6
+
+	switch point {
+	case 8:
+		fmt.Println("perfect")
+	case 7:
+		fmt.Print("awesome")
+	default:
+		fmt.Println("not bad")
 	}
+
 }
---
+++ cmd/syntax/loop/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("Angka", i)
+	// }
+
+	// var i = 0
+	//
+	// for i < 5 {
+	// 	fmt.Println("Angka", i)
+	// 	i++
+	// }
+
+	// var xs = "123"
+	// for i, v := range xs {
+	// 	fmt.Println("Index=", i, "Value", v)
+	// }
+	//
+	// var ys = [5]int{10, 20, 30, 40, 50}
+	//
+	// for _, v := range ys {
+	// 	fmt.Println("Value", v)
+	// }
+	//
+	// var zs = ys[0:2] // slice
+	//
+	// for _, v := range zs {
+	// 	fmt.Print("Value", v)
+	// }
+	//
+	// var kvs = map[byte]int{'a': 0, 'b': 1, 'c': 2} // map
+	//
+	// for k, v := range kvs {
+	// 	fmt.Println("Key=", k, "Value", v)
+	// }
+	//
+	// for range kvs {
+	// 	fmt.Println("Done")
+	// }
+	//
+	// for i := range 5 {
+	// 	fmt.Print(i)
+	// }
+
+	// break and continue
+
+	// for i := 1; i <= 10; i++ {
+	// 	if i%2 == 1 {
+	// 		continue
+	// 	}
+	//
+	// 	if i > 8 {
+	// 		break
+	// 	}
+	//
+	// 	fmt.Println("Angka", i)
+	// }
+
+}
